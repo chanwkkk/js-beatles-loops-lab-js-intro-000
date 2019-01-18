f81d416a0b1acb2131de6e5272bc5f32b8f3c0e0
Yingqi Chen <chanwk18@hotmail.com>
2019-01-18 07:22:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array=new Array();
+  for(var i=0;i<musicians.length;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }return array;
+}
+
+function johnLennonFacts(array){
+  var i=0;
+  var array2=new Array();
+  while (i<array.length){
+    array2.push(`${array[i]}!!!`);
+    i++;
+  }return array2;
+}
